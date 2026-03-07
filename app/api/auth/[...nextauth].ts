30ce6ff1cc4cc82e60d3ea51386cb3348e34adc4
khanakorn kositkhongchana <khanakorn01@gmail.com>
2026-03-07 15:16:18 +0700

    feat: initial commit

---
+++ app/api/auth/[...nextauth].ts
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth";
+import { PrismaAdapter } from "@auth/prisma-adapter";
+import GoogleProvider from "next-auth/providers/google";
+import { prisma } from "@/prisma.config";
+export const authOptions = {
+  adapter: PrismaAdapter(prisma),
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+};
+export default NextAuth(authOptions);
